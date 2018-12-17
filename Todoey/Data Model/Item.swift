4ef33c7d4549bb7d19f64527d0126d71c8d75e81
AJ Adams <ajadams@ajs-macbook-pro.local>
2018-12-17 18:58:23 -0500

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by AJ Adams on 12/17/18.
+//  Copyright © 2018 AJ Adams. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
